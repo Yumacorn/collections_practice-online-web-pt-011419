f76e0eea24ef3d3aefee536609110e6a33fa6390
Ryan Mui <reachryanmui@gmail.com>
2019-02-07 01:38:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  sortedarray = array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 1, 2)
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  temphold = array[1]
+  array[1] = array[2]
+  array[2] = temphold
+  array
+end
+
+def reverse_array(array)
+  reversedarray = []
+  reversedarray << array.each {|element| array.shift}
+  reversedarray
+end
